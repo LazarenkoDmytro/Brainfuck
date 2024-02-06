db633ffeca26ee84e428252e47a995c7d4c7a38a
LazarenkoDmytro <lazarenkodmytro.work@gmail.com>
2024-02-06 17:05:56 +0200

    Implemented the PrintCurrentCellParser class.

---
+++ src/io/javaclasses/brainfuck/classes/PrintCurrentCellParser.java
@@ -0,0 +1,13 @@
+package io.javaclasses.brainfuck.classes;
+
+/**
+ * A parser of the interpreter.
+ * Implements the {@link Parser} interface.
+ * Parses the PrintCurrentCell symbol.
+ */
+public class PrintCurrentCellParser implements Parser {
+    @Override
+    public Command parse(String brainfuckExpression, MutableInt index) {
+        return new PrintCurrentCell();
+    }
+}
