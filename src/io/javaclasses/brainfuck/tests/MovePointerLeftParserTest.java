fa10a31e977d29dbef3d92f89dd1fb505bf1aaa0
LazarenkoDmytro <lazarenkodmytro.work@gmail.com>
2024-02-06 16:39:42 +0200

    Implemented the MovePointerLeftParserTest class. It tests the logic of MovePointerLeftParser class.

---
+++ src/io/javaclasses/brainfuck/tests/MovePointerLeftParserTest.java
@@ -0,0 +1,30 @@
+package io.javaclasses.brainfuck.tests;
+
+import io.javaclasses.brainfuck.classes.MovePointerLeft;
+import io.javaclasses.brainfuck.classes.MovePointerLeftParser;
+import io.javaclasses.brainfuck.classes.MutableInt;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
+
+/**
+ * Tests the logic of the {@link MovePointerLeftParser} class.
+ */
+public class MovePointerLeftParserTest {
+    private MovePointerLeftParser movePointerLeftParser;
+    private String brainfuckExpression;
+    private MutableInt mutableInt;
+
+    @BeforeEach
+    void setUp() {
+        movePointerLeftParser = new MovePointerLeftParser();
+        brainfuckExpression = "><+-";
+        mutableInt = new MutableInt(0);
+    }
+
+    @Test
+    void testParse() {
+        assertInstanceOf(MovePointerLeft.class, movePointerLeftParser.parse(brainfuckExpression, mutableInt));
+    }
+}
