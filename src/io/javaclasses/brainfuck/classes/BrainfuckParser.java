28e0ef26c1624a4849290035e4fdb19679a99c77
LazarenkoDmytro <lazarenkodmytro.work@gmail.com>
2024-02-06 18:17:30 +0200

    Implemented the BrainfuckParser class.

---
+++ src/io/javaclasses/brainfuck/classes/BrainfuckParser.java
@@ -0,0 +1,25 @@
+package io.javaclasses.brainfuck.classes;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Parser of the String in Brainfuck standard.
+ * Extracts the Commands from the expression.
+ */
+public class BrainfuckParser {
+    private final ParserManager parserManager = new ParserManager();
+
+    public List<Command> parse(String brainfuckExpression) {
+        List<Command> commands = new ArrayList<>();
+        for (MutableInt index = new MutableInt(0); index.value() < brainfuckExpression.length(); index.increment()) {
+            char currentCharacter = brainfuckExpression.charAt(index.value());
+            Parser parser = parserManager.parser(currentCharacter);
+
+            Command command = parser.parse(brainfuckExpression, index);
+            commands.add(command);
+        }
+
+        return commands;
+    }
+}
