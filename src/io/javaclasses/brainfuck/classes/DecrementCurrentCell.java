9903371c40597c236100499fab37fb85807814f6
LazarenkoDmytro <lazarenkodmytro.work@gmail.com>
2024-02-06 13:18:59 +0200

    Implemented the DecrementCurrentCell class.

---
+++ src/io/javaclasses/brainfuck/classes/DecrementCurrentCell.java
@@ -0,0 +1,13 @@
+package io.javaclasses.brainfuck.classes;
+
+/**
+ * A command of the interpreter.
+ * Implements the {@link Command} interface.
+ * Decrements the value of the current cell in the memory.
+ */
+public class DecrementCurrentCell implements Command {
+    @Override
+    public void execute(Memory memory) {
+        memory.decrementCurrentCell();
+    }
+}
