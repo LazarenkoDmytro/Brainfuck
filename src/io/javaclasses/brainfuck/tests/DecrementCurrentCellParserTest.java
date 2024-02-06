5b04cf3c27b8f649d6d254ebc6da89a0cc3ba9af
LazarenkoDmytro <lazarenkodmytro.work@gmail.com>
2024-02-06 16:53:35 +0200

    Implemented the DecrementCurrentCellParserTest class. It tests the logic of DecrementCurrentCellParser class.

---
+++ src/io/javaclasses/brainfuck/tests/DecrementCurrentCellParserTest.java
@@ -0,0 +1,30 @@
+package io.javaclasses.brainfuck.tests;
+
+import io.javaclasses.brainfuck.classes.DecrementCurrentCell;
+import io.javaclasses.brainfuck.classes.DecrementCurrentCellParser;
+import io.javaclasses.brainfuck.classes.MutableInt;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
+
+/**
+ * Tests the logic of the {@link DecrementCurrentCellParser} class.
+ */
+public class DecrementCurrentCellParserTest {
+    private DecrementCurrentCellParser decrementCurrentCellParser;
+    private String brainfuckExpression;
+    private MutableInt mutableInt;
+
+    @BeforeEach
+    void setUp() {
+        decrementCurrentCellParser = new DecrementCurrentCellParser();
+        brainfuckExpression = "><+-.[";
+        mutableInt = new MutableInt(3);
+    }
+
+    @Test
+    void testParse() {
+        assertInstanceOf(DecrementCurrentCell.class, decrementCurrentCellParser.parse(brainfuckExpression, mutableInt));
+    }
+}
